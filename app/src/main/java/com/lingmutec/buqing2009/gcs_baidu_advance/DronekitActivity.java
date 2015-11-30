2f1fe47ce7e5916170c72cd5cf2d165afabd474c
buqing2009 <buqing2009@gmail.com>
2015-11-30 14:15:05 +0800

    Initial commit

---
+++ app/src/main/java/com/lingmutec/buqing2009/gcs_baidu_advance/DronekitActivity.java
@@ -0,0 +1,17 @@
+package com.lingmutec.buqing2009.gcs_baidu_advance;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by buqing2009 on 15-11-26.
+ */
+public class DronekitActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dronekit_activity);
+    }
+
+}
